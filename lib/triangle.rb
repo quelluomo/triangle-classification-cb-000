7f05ceb8b8da024d113d87e876c8db68363f6094
Matteo Muscas <thatman.j@gmail.com>
2018-12-18 16:13:18 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,32 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if a<=0 || b<=0 || c<=0 || a + b <= c || a + c <= b || b + c <= a
+      begin
+        raise TriangleError
+      end
+    else
+      if a==b && b==c
+        :equilateral
+      elsif a==b || b==c || a==c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Ano, sumimasen. Toraianguru erro da!"
+    end
+  end
+
 end
