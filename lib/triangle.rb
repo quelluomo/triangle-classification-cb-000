d9170fd1d84c50f44e6a48393be7a82e1ea95d82
Matteo Muscas <thatman.j@gmail.com>
2018-12-18 15:58:34 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,18 +8,23 @@ class Triangle
   end
 
   def kind
-    if a==b && b==c && a==c
-      :equilateral
-    elsif a==b || b==c || a==c
-      :isosceles
+    if a==0 || b==0 || c==0
+      begin
+        raise TriangleError
+      end
     else
-      :scalene
+      if a==b && b==c && a==c
+        :equilateral
+      elsif a==b || b==c || a==c
+        :isosceles
+      else
+        :scalene
+      end
     end
   end
 
-
   class TriangleError < StandardError
-    def message 
+    def message
       "Ano, sumimasen. Toraianguru erro da!"
     end
   end
